09b666810673aeb3a195dbcbd22254388eff2bc6
Babar Ali <babar@evolverstech.com>
2019-03-13 12:29:34 +0500

    SignIn, SignUp & OTP is completed with APIs integration

--- app/src/main/java/test/invoicegenerator/databaseutilities/DBHelper.java
+++ app/src/main/java/test/invoicegenerator/databaseutilities/DBHelper.java
@@ -37,7 +37,7 @@ public class DBHelper extends SQLiteOpenHelper {
     private HashMap hp;
 
     public DBHelper(Context context) {
-        super(context, DATABASE_NAME, null, 14);
+        super(context, DATABASE_NAME, null, 17);
     }
 
     @Override
